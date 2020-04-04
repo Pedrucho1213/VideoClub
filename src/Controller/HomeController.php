3d10ae99415e7dde58e01c59b42cfec4e8bb92b2
Léo LEPLOMB <leo.leplomb@gmail.com>
2020-04-04 23:59:07 +0200

    Add the possibility to add a movie

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Films;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 class HomeController extends AbstractController
 {
     /**
@@ -17,6 +22,48 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+    // début du rajout léo <-- a supprimer (et la fin aussi)
+        /**
+     * @Route("/movie/new", name="movie_new")
+     */
+    public function newFilm (Request $request, EntityManagerInterface $entityManager) {
+        $film = new Film();
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $form = $this->createFormBuilder($film)
+                     ->add('Title')
+                     ->add('OriginaTitle')
+                     ->add('OriginalLanguage')
+                     ->add('Genre')
+                     ->add('ReleaseData')
+                     ->add('PosterPath')
+                     ->add('Overview')
+                     ->add('VideoPath')
+                     ->add('Category', ChoiceType::class, [
+                         'categories' => [
+                             'film' => 'film',
+                             'serie' => 'serie',
+                         ]
+                     ])
+                     ->getForm();   
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($film);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('detailMovie', [
+                'id' => $film->getId()
+                ]);
+        }
+
+
+        return $this->render('home/new.movie.html.twig', [
+            'formFilm' => $form->createView()
+        ]);
+    }
+    //fin du rajout de léo
 
     /**
      * @Route("/home/{category}", name="movie")
